236baeae56b1b6f40e77157a18b75631b39bb3bb
Amovane <amovane@163.com>
2022-08-25 10:43:19 +0800

    Add CLI skeleton

---
+++ crypto_cli/src/command.rs
@@ -0,0 +1,16 @@
+use clap::{Args, Parser, Subcommand};
+
+
+#[derive(Debug, Parser)]
+#[clap(name = "coinfo")]
+#[clap(about = "A CLI tool for everything about cryptocurrencies")]
+pub struct Cli {
+    #[clap(subcommand)]
+    pub command: Commands,
+}
+
+#[derive(Debug, Subcommand)]
+pub enum Commands {
+    #[clap(arg_required_else_help = true)]
+    Price { currencies: String },
+}
--- crypto_cli/src/main.rs
+++ crypto_cli/src/main.rs
@@ -1,21 +1,31 @@
+use clap::Parser;
+use command::Commands;
 use exchanges::{Binance, Exchange};
+mod command;
 mod exchanges;
 
 fn main() {
-    match Binance::get_instrument("BTCUSDT") {
-        Ok(data) => {
-            println!("{:?}", data);
-        }
-        Err(e) => {
-            println!("{:?}", e);
-        }
-    }
-    match Binance::get_instruments(vec!["BTCUSDT", "ETHUSDT"]) {
-        Ok(data) => {
-            println!("{:?}", data);
-        }
-        Err(e) => {
-            println!("{:?}", e);
+    let args = command::Cli::parse();
+    match args.command {
+        Commands::Price { currencies } => {
+            let currencies: Vec<&str> = currencies.split(",").collect();
+            println!("{:?}", currencies)
         }
     }
+    // match Binance::get_instrument("BTCUSDT") {
+    //     Ok(data) => {
+    //         println!("{:?}", data);
+    //     }
+    //     Err(e) => {
+    //         println!("{:?}", e);
+    //     }
+    // }
+    // match Binance::get_instruments(vec!["BTCUSDT", "ETHUSDT"]) {
+    //     Ok(data) => {
+    //         println!("{:?}", data);
+    //     }
+    //     Err(e) => {
+    //         println!("{:?}", e);
+    //     }
+    // }
 }
