905f0caeb060d5401b523ba459c89178d9e34277
Amovane <amovane@163.com>
2022-08-26 10:30:06 +0800

    Migrate to comfy-table

--- coinfo/src/display.rs
+++ coinfo/src/display.rs
@@ -2,37 +2,39 @@ use crate::{
     aggregators::CommunityInfo,
     exchanges::{Instrument, QUOTE},
 };
-use prettytable::{row, Table};
+use comfy_table::{ContentArrangement, Table};
 
 pub fn display_instruments(instruments: Vec<Instrument>) {
     let mut table = Table::new();
-    table.set_titles(row![
+    table.set_header(vec![
         "Symbol",
         "Exchange",
-        format!("Price ({})", QUOTE.to_string()),
+        &format!("Price ({})", QUOTE.to_string()),
         "PriceChange%",
-        format!("24H Volume ({})", QUOTE.to_string())
+        &format!("24H Volume ({})", QUOTE.to_string()),
     ]);
     for instr in instruments.iter() {
-        table.add_row(row![
-            instr.symbol,
-            instr.ex_name,
-            instr.price,
-            instr.price_24h_change_percent,
-            instr.volume
+        table.add_row(vec![
+            instr.symbol.clone(),
+            instr.ex_name.clone(),
+            instr.price.to_string(),
+            instr.price_24h_change_percent.to_string(),
+            instr.volume.to_string(),
         ]);
     }
-    table.printstd();
+    println!("{table}");
 }
 
 pub fn display_community_info(info: CommunityInfo) {
     let mut table = Table::new();
-    table.set_titles(row![
+    table.set_content_arrangement(ContentArrangement::Dynamic);
+
+    table.set_header(vec![
         "Currency",
         "Homepage",
         "Explorer",
         "Opensource",
-        "Description"
+        "Description",
     ]);
     let readmore = format!("https://www.coingecko.com/en/coins/{}", info.id);
     let description = info
@@ -42,12 +44,12 @@ pub fn display_community_info(info: CommunityInfo) {
         .collect::<Vec<&str>>()
         .join(" ");
 
-    table.add_row(row![
+    table.add_row(vec![
         info.symbol,
         info.homepage.join("\n"),
         info.explorer.join("\n"),
         info.opensource.join("\n"),
-        format!("{}\nRead more on {}", description, readmore)
+        format!("{}...\n\nRead more on {}", description, readmore),
     ]);
-    table.printstd();
+    println!("{table}")
 }
